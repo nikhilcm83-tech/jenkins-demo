2b53a53d6814822e696f14abb1f30969b7dbac50
Nikhil <nikhilcm83@gmail.com>
2026-05-01 05:03:21 +0000

    update app and dockerfile

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("CI/cd pipline working")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Pipeline Working - App Running!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
